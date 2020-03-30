22632e9b4f3728aa88ca052d718fdd237aa91729
tibbi <tibor@kaputa.sk>
2020-03-30 22:39:53 +0200

    adding some licenses

--- app/src/main/kotlin/com/simplemobiletools/voicerecorder/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/voicerecorder/activities/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.simplemobiletools.commons.extensions.*
-import com.simplemobiletools.commons.helpers.PERMISSION_RECORD_AUDIO
-import com.simplemobiletools.commons.helpers.PERMISSION_WRITE_STORAGE
-import com.simplemobiletools.commons.helpers.isQPlus
+import com.simplemobiletools.commons.helpers.*
 import com.simplemobiletools.commons.models.FAQItem
 import com.simplemobiletools.voicerecorder.BuildConfig
 import com.simplemobiletools.voicerecorder.R
@@ -165,7 +163,7 @@ class MainActivity : SimpleActivity() {
     }
 
     private fun launchAbout() {
-        val licenses = 0
+        val licenses = LICENSE_EVENT_BUS or LICENSE_AUDIO_RECORD_VIEW
 
         val faqItems = arrayListOf(
             FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
