244250cc2bede2ba574cd8ee4091c723bda949bc
tibbi <tibor@kaputa.sk>
2020-04-17 22:57:26 +0200

    try improving the recording quality

--- app/src/main/kotlin/com/simplemobiletools/voicerecorder/services/RecorderService.kt
+++ app/src/main/kotlin/com/simplemobiletools/voicerecorder/services/RecorderService.kt
@@ -76,6 +76,8 @@ class RecorderService : Service() {
             setAudioSource(MediaRecorder.AudioSource.CAMCORDER)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
             setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setAudioEncodingBitRate(128000)
+            setAudioSamplingRate(44100)
 
             try {
                 if (!isQPlus() && isPathOnSD(currFilePath)) {
