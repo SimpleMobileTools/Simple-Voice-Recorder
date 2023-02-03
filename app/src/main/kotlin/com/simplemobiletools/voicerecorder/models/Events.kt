9a4dcd1d7d131ec155e5408fec3f3b61e4e55921
Naveen <snaveen935@gmail.com>
2023-02-04 02:06:49 +0530

    Handle record audio intent
    There's a related Intent-extra used to define the maximum file size `MediaStore.Audio.Media.EXTRA_MAX_BYTES` but it's not yet handled by the app (I doubt many apps use it)

--- app/src/main/kotlin/com/simplemobiletools/voicerecorder/models/Events.kt
+++ app/src/main/kotlin/com/simplemobiletools/voicerecorder/models/Events.kt
@@ -1,8 +1,11 @@
 package com.simplemobiletools.voicerecorder.models
 
+import android.net.Uri
+
 class Events {
     class RecordingDuration internal constructor(val duration: Int)
     class RecordingStatus internal constructor(val status: Int)
     class RecordingAmplitude internal constructor(val amplitude: Int)
     class RecordingCompleted internal constructor()
+    class RecordingSaved internal constructor(val uri: Uri?)
 }
