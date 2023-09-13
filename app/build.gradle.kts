57ee97e060ffcee6fe5682a9c9eb0ffebaff1df2
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-09-13 15:17:32 +0200

    Clean up gradle deprecation warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
     alias(libs.plugins.android)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.ksp)
+    base
+}
+
+base {
+    archivesName.set("voice-recorder")
 }
 
 val keystorePropertiesFile: File = rootProject.file("keystore.properties")
@@ -23,7 +28,6 @@ android {
         targetSdk = project.libs.versions.app.build.targetSDK.get().toInt()
         versionName = project.libs.versions.app.version.versionName.get()
         versionCode = project.libs.versions.app.version.versionCode.get().toInt()
-        setProperty("archivesBaseName", "voice-recorder")
         vectorDrawables.useSupportLibrary = true
     }
 
