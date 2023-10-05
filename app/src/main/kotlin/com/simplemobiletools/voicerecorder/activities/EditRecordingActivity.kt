7290b11fab98d708c2c485827029e6379a4483a2
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-10-05 11:24:31 +0200

    Move editing controls to bottom

--- app/src/main/kotlin/com/simplemobiletools/voicerecorder/activities/EditRecordingActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/voicerecorder/activities/EditRecordingActivity.kt
@@ -10,11 +10,10 @@ import android.os.Looper
 import android.os.PowerManager
 import android.provider.DocumentsContract
 import android.widget.SeekBar
-import androidx.core.view.children
+import androidx.core.view.isVisible
 import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.commons.helpers.NavigationIcon
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
-import com.simplemobiletools.voicerecorder.R
 import com.simplemobiletools.voicerecorder.databinding.ActivityEditRecordingBinding
 import com.simplemobiletools.voicerecorder.extensions.getAllRecordings
 import com.simplemobiletools.voicerecorder.helpers.getAudioFileContentUri
@@ -45,7 +44,6 @@ class EditRecordingActivity : SimpleActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditRecordingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupOptionsMenu()
 
         updateMaterialActivityViews(binding.mainCoordinator, binding.recordingVisualizer, useTransparentNavigation = false, useTopSearchMenu = false)
 
@@ -65,13 +63,26 @@ class EditRecordingActivity : SimpleActivity() {
 
 //        binding.recordingVisualizer.waveProgressColor = getProperPrimaryColor()
 //        binding.recordingVisualizer.setSampleFrom(recording.path)
+        val controls = listOf(binding.playerControlsWrapper.cutBtn, binding.playerControlsWrapper.clearBtn, binding.playerControlsWrapper.resetBtn)
+        listOf(binding.playerControlsWrapper.cutBtn, binding.playerControlsWrapper.clearBtn, binding.playerControlsWrapper.resetBtn).forEach {
+            it.isVisible = false
+        }
+
+        binding.playerControlsWrapper.cutBtn.setOnClickListener { cutSelection() }
+        binding.playerControlsWrapper.clearBtn.setOnClickListener { clearSelection() }
+        binding.playerControlsWrapper.resetBtn.setOnClickListener { resetEditing() }
+
         binding.recordingVisualizer.chunkColor = getProperPrimaryColor()
         binding.recordingVisualizer.recreate()
         binding.recordingVisualizer.editListener = {
             if (binding.recordingVisualizer.startPosition >= 0f) {
-                binding.editToolbar.menu.children.forEach { it.isVisible = true }
+                playSelection()
+                controls.forEach { it.isVisible = true }
             } else {
-                binding.editToolbar.menu.children.forEach { it.isVisible = false }
+                controls.forEach { it.isVisible = false }
+                if (recording != currentRecording) {
+                    binding.playerControlsWrapper.resetBtn.isVisible = true
+                }
             }
         }
         updateVisualization()
@@ -128,111 +139,13 @@ class EditRecordingActivity : SimpleActivity() {
         val properPrimaryColor = getProperPrimaryColor()
         updateTextColors(binding.mainCoordinator)
 
+        val textColor = getProperTextColor()
+        arrayListOf(binding.playerControlsWrapper.cutBtn, binding.playerControlsWrapper.clearBtn, binding.playerControlsWrapper.resetBtn).forEach {
+            it.applyColorFilter(textColor)
+        }
+
         binding.playerControlsWrapper.playPauseBtn.background.applyColorFilter(properPrimaryColor)
         binding.playerControlsWrapper.playPauseBtn.setImageDrawable(getToggleButtonIcon(false))
-    }
-
-    private fun setupOptionsMenu() {
-        binding.editToolbar.inflateMenu(R.menu.menu_edit)
-//        binding.settingsToolbar.toggleHideOnScroll(false)
-//        binding.settingsToolbar.setupMenu()
-
-//        binding.settingsToolbar.onSearchOpenListener = {
-//            if (binding.viewPager.currentItem == 0) {
-//                binding.viewPager.currentItem = 1
-//            }
-//        }
-
-//        binding.settingsToolbar.onSearchTextChangedListener = { text ->
-//            getPagerAdapter()?.searchTextChanged(text)
-//        }
-
-        binding.editToolbar.menu.children.forEach { it.isVisible = false }
-        binding.editToolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.play -> {
-                    val start = binding.recordingVisualizer.startPosition
-                    val end = binding.recordingVisualizer.endPosition
-
-                    val startMillis = start * currentRecording.duration
-                    val durationMillis = (end - start) * currentRecording.duration
-                    val startMillisPart = String.format("%.3f", startMillis - startMillis.toInt()).replace("0.", "")
-                    val durationMillisPart = String.format("%.3f", durationMillis - durationMillis.toInt()).replace("0.", "")
-                    val startFormatted = (startMillis.toInt()).getFormattedDuration(true) + ".$startMillisPart"
-                    val durationFormatted = (durationMillis.toInt()).getFormattedDuration(true) + ".$durationMillisPart"
-                    modifyAudioFile(currentRecording)
-                        .cutAudio(startFormatted, durationFormatted) {
-                            progressStart = binding.recordingVisualizer.startPosition
-                            playRecording(it.path, null, it.name, durationMillis.toInt(), true)
-                        }
-                        .release()
-//                    playRecording()
-                }
-                R.id.cut -> {
-                    val start = binding.recordingVisualizer.startPosition
-                    val end = binding.recordingVisualizer.endPosition
-
-                    val startMillis = start * currentRecording.duration
-                    val endMillis = end * currentRecording.duration
-                    val realEnd = (1 - end) * currentRecording.duration
-                    val startMillisPart = String.format("%.3f", startMillis - startMillis.toInt()).replace("0.", "")
-                    val endMillisPart = String.format("%.3f", endMillis - endMillis.toInt()).replace("0.", "")
-                    val realEndMillisPart = String.format("%.3f", realEnd - realEnd.toInt()).replace("0.", "")
-                    val startFormatted = (startMillis.toInt()).getFormattedDuration(true) + ".$startMillisPart"
-                    val endFormatted = (endMillis.toInt()).getFormattedDuration(true) + ".$endMillisPart"
-                    val realEndFormatted = (realEnd.toInt()).getFormattedDuration(true) + ".$realEndMillisPart"
-
-                    var leftPart: File? = null
-                    var rightPart: File? = null
-
-                    fun merge() {
-                        if (leftPart != null && rightPart != null) {
-                            ensureBackgroundThread {
-                                val tempFile = File.createTempFile("${currentRecording.title}.edit.", ".${currentRecording.title.getFilenameExtension()}", cacheDir)
-                                AudioTool.getInstance(this)
-                                    .joinAudios(arrayOf(leftPart, rightPart), tempFile.path) {
-                                        runOnUiThread {
-                                            currentRecording = Recording(-1, it.name, it.path, it.lastModified().toInt(), (startMillis + realEnd).toInt(), it.getProperSize(false).toInt())
-                                            updateVisualization()
-                                            playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
-                                        }
-                                    }
-                            }
-                        }
-                    }
-
-                    modifyAudioFile(currentRecording)
-                        .cutAudio("00:00:00", startFormatted) {
-                            leftPart = it
-                            merge()
-                        }
-                    modifyAudioFile(currentRecording)
-                        .cutAudio(endFormatted, realEndFormatted) {
-                            rightPart = it
-                            merge()
-                        }
-                }
-//                R.id.save -> {
-//                    binding.recordingVisualizer.clearEditing()
-//                    currentRecording = recording
-//                    playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
-//                }
-                R.id.clear -> {
-                    progressStart = 0f
-                    binding.recordingVisualizer.clearEditing()
-                    playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
-                }
-                R.id.reset -> {
-                    progressStart = 0f
-                    binding.recordingVisualizer.clearEditing()
-                    currentRecording = recording
-                    updateVisualization()
-                    playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
-                }
-                else -> return@setOnMenuItemClickListener false
-            }
-            return@setOnMenuItemClickListener true
-        }
     }
 
     private fun initMediaPlayer() {
@@ -306,6 +219,83 @@ class EditRecordingActivity : SimpleActivity() {
         })
     }
 
+    private fun clearSelection() {
+        progressStart = 0f
+        binding.recordingVisualizer.clearEditing()
+        playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
+    }
+
+    private fun resetEditing() {
+        progressStart = 0f
+        binding.recordingVisualizer.clearEditing()
+        currentRecording = recording
+        updateVisualization()
+        playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
+    }
+
+    private fun playSelection() {
+        val start = binding.recordingVisualizer.startPosition
+        val end = binding.recordingVisualizer.endPosition
+
+        val startMillis = start * currentRecording.duration
+        val durationMillis = (end - start) * currentRecording.duration
+        val startMillisPart = String.format("%.3f", startMillis - startMillis.toInt()).replace("0.", "")
+        val durationMillisPart = String.format("%.3f", durationMillis - durationMillis.toInt()).replace("0.", "")
+        val startFormatted = (startMillis.toInt()).getFormattedDuration(true) + ".$startMillisPart"
+        val durationFormatted = (durationMillis.toInt()).getFormattedDuration(true) + ".$durationMillisPart"
+        modifyAudioFile(currentRecording)
+            .cutAudio(startFormatted, durationFormatted) {
+                progressStart = binding.recordingVisualizer.startPosition
+                playRecording(it.path, null, it.name, durationMillis.toInt(), true)
+            }
+            .release()
+    }
+
+    private fun cutSelection() {
+        val start = binding.recordingVisualizer.startPosition
+        val end = binding.recordingVisualizer.endPosition
+
+        val startMillis = start * currentRecording.duration
+        val endMillis = end * currentRecording.duration
+        val realEnd = (1 - end) * currentRecording.duration
+        val startMillisPart = String.format("%.3f", startMillis - startMillis.toInt()).replace("0.", "")
+        val endMillisPart = String.format("%.3f", endMillis - endMillis.toInt()).replace("0.", "")
+        val realEndMillisPart = String.format("%.3f", realEnd - realEnd.toInt()).replace("0.", "")
+        val startFormatted = (startMillis.toInt()).getFormattedDuration(true) + ".$startMillisPart"
+        val endFormatted = (endMillis.toInt()).getFormattedDuration(true) + ".$endMillisPart"
+        val realEndFormatted = (realEnd.toInt()).getFormattedDuration(true) + ".$realEndMillisPart"
+
+        var leftPart: File? = null
+        var rightPart: File? = null
+
+        fun merge() {
+            if (leftPart != null && rightPart != null) {
+                ensureBackgroundThread {
+                    val tempFile = File.createTempFile("${currentRecording.title}.edit.", ".${currentRecording.title.getFilenameExtension()}", cacheDir)
+                    AudioTool.getInstance(this)
+                        .joinAudios(arrayOf(leftPart, rightPart), tempFile.path) {
+                            runOnUiThread {
+                                currentRecording = Recording(-1, it.name, it.path, it.lastModified().toInt(), (startMillis + realEnd).toInt(), it.getProperSize(false).toInt())
+                                updateVisualization()
+                                playRecording(currentRecording.path, currentRecording.id, currentRecording.title, currentRecording.duration, true)
+                            }
+                        }
+                }
+            }
+        }
+
+        modifyAudioFile(currentRecording)
+            .cutAudio("00:00:00", startFormatted) {
+                leftPart = it
+                merge()
+            }
+        modifyAudioFile(currentRecording)
+            .cutAudio(endFormatted, realEndFormatted) {
+                rightPart = it
+                merge()
+            }
+    }
+
     private fun setupProgressTimer() {
         progressTimer.cancel()
         progressTimer = Timer()
@@ -359,17 +349,6 @@ class EditRecordingActivity : SimpleActivity() {
     private fun getToggleButtonIcon(isPlaying: Boolean): Drawable {
         val drawable = if (isPlaying) com.simplemobiletools.commons.R.drawable.ic_pause_vector else com.simplemobiletools.commons.R.drawable.ic_play_vector
         return resources.getColoredDrawableWithColor(drawable, getProperPrimaryColor().getContrastColor())
-    }
-
-    private fun skip(forward: Boolean) {
-//        val curr = player?.currentPosition ?: return
-//        var newProgress = if (forward) curr + FAST_FORWARD_SKIP_MS else curr - FAST_FORWARD_SKIP_MS
-//        if (newProgress > player!!.duration) {
-//            newProgress = player!!.duration
-//        }
-//
-//        player!!.seekTo(newProgress)
-//        resumePlayback()
     }
 
     private fun getIsPlaying() = player?.isPlaying == true
